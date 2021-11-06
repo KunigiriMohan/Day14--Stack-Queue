5dda621938d975c3dde265958a9b697498b9ee40
KunigiMohan <kmohanr5@gmail.com>
2021-11-06 11:00:02 +0530

    Added : Dequeue from begining

--- src/Queue.java
+++ src/Queue.java
@@ -42,4 +42,21 @@ class Queue {
             }
         }
     }
+    void dequeue()
+    {
+        // If queue is empty, return NULL.
+        if (this.front == null) {
+            return;
+        }
+
+        // Store previous front and move front one node ahead
+        Node temp = this.front;
+        this.front = this.front.next;
+
+        // If front becomes NULL, then change rear also as NULL
+        if (this.front == null)
+        {
+            this.rear = null;
+        }
+    }
 }
