0110d6631e5fd54130d5ae3d81be023b0342f650
KunigiMohan <kmohanr5@gmail.com>
2021-11-06 08:43:35 +0530

    Added : Ability pop and peak from the stack of 56,30 and 70.

--- src/StackUsingLinkedlist.java
+++ src/StackUsingLinkedlist.java
@@ -33,18 +33,48 @@ class StackUsingLinkedlist {
 
         top = temp;
     }
+    public int peek()
+    {
+        // check for empty stack
+        if (!isEmpty()) {
+            return top.data;
+        }
+        else {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+    }
+    // Utility function to check if the stack is empty or not
+    public boolean isEmpty()
+    {
+        return top == null;
+    }
+
+    // Utility function to pop top element from the stack
+    public void pop() // remove at the beginning
+    {
+        // check for stack underflow
+        if (top == null) {
+            System.out.print("\nStack Underflow");
+            return;
+        }
+
+        // update the top pointer to point to the next node
+        top = (top).link;
+    }
+
 
     public void display()
     {
         if (top == null) {
-            System.out.printf("Stack Underflow");
+            System.out.println("Stack Underflow");
             exit(1);
         }
         else {
             Node temp = top;
             while (temp != null)
             {
-                System.out.print(temp.data+"  ");
+                System.out.println(temp.data+"  ");
                 temp = temp.link;
             }
         }
